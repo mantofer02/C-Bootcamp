225deeb724e7bbc29c7743e2ddf49a4558784dd1
mantofer <mantofer2000@gmail.com>
2021-07-18 21:57:53 -0600

    Coffee

---
+++ 33/coffeeCups.cpp
@@ -0,0 +1,30 @@
+/*
+  Copyright 2021 Marco Ferraro
+*/
+
+#include <string>
+#include <iostream>
+#include <math.h>
+
+/*
+  For each 6 cups I buy, I get a 7th cup free. Create a function 
+  that take n cups bought and return the total of cups I would get.
+
+*/
+
+static int coffeeCups(int n) {
+  n+= (int)floor(n/6);
+  return n;
+}
+
+
+int main(int argc, char* argv[]) {
+  int n = 0;
+  if (argc > 1) {
+    n = atoi(argv[1]);
+  }
+  
+  std::cout << coffeeCups(n) << std::endl;
+  
+  return 0;
+}
